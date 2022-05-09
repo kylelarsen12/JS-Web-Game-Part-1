7cd2209ab76fa49e594424ebc635e4e149c67064
unknown <kylelarsen12@gmail.com>
2022-05-09 17:04:26 -0400

    add backgroundBoxes function for bonus activity

--- index.js
+++ index.js
@@ -9,12 +9,36 @@ function newImg(source, left, bottom) {
   return image;
 }
 
-function newItem(url, left, bottom) {
-  let item = newImg(url, left, bottom);
+function newItem(source, left, bottom) {
+  let item = newImg(source, left, bottom);
   item.addEventListener("dblclick", function () {
     item.remove();
   });
 }
+
+function backgroundBoxes(source, left, bottom, userWidth, userHeight) {
+  for (let i = 0; i < userWidth; i++) {
+    for (let j = 0; j < userHeight; j++) {
+      newImg(source, left + i * 100, bottom + j * 100);
+    }
+  }
+}
+
+//Call functions to create game
+backgroundBoxes(
+  "./assets/grass.png",
+  0,
+  0,
+  window.innerWidth / 100,
+  window.innerHeight / 100
+);
+backgroundBoxes(
+  "./assets/sky.png",
+  0,
+  window.innerHeight * 0.75,
+  window.innerWidth / 100,
+  window.innerHeight / 100
+);
 
 newImg("./assets/green-character.gif", 100, 100);
 newImg("./assets/pine-tree.png", 450, 200);
@@ -22,19 +46,6 @@ newImg("./assets/tree.png", 200, 300);
 newImg("./assets/pillar.png", 350, 100);
 newImg("./assets/crate.png", 150, 200);
 newImg("./assets/well.png", 500, 425);
-
-/*
-let sword = document.createElement("img");
-sword.src = "./assets/sword.png";
-sword.style.position = "fixed";
-sword.style.left = "500px";
-sword.style.bottom = "405px";
-document.body.append(sword);
-
-sword.addEventListener("click", function () {
-  sword.remove();
-});
-*/
 
 newItem("./assets/sword.png", 500, 405);
 newItem("./assets/sheild.png", 165, 185);
